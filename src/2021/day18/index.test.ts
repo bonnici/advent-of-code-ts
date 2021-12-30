5064471c5bb83c66bf2cf35e6c4e8242f082b803
Nick Marsh <nickmarsh@gmail.com>
2021-12-30 15:47:46 +1000

    Day 18

--- src/2021/day18/index.test.ts
+++ src/2021/day18/index.test.ts
@@ -1,36 +1,334 @@
 import { Pair } from './index';
 
-describe('constructor', () => {
+describe('fromString', () => {
 	test('simple', () => {
-		const pair = new Pair('[1,2]');
+		const pair = Pair.fromString('[1,2]');
 
+		expect(pair.parent).toBeUndefined();
+		expect(pair.depth).toEqual(0);
 		expect(pair.value).toBeUndefined();
+		expect(pair.left).not.toBeUndefined();
+		expect(pair.right).not.toBeUndefined();
 
+		expect(pair.left?.parent).toBe(pair);
+		expect(pair.left?.depth).toEqual(1);
 		expect(pair.left?.value).toEqual(1);
 		expect(pair.left?.left).toBeUndefined();
 		expect(pair.left?.right).toBeUndefined();
 
+		expect(pair.right?.parent).toBe(pair);
+		expect(pair.right?.depth).toEqual(1);
 		expect(pair.right?.value).toEqual(2);
 		expect(pair.right?.left).toBeUndefined();
 		expect(pair.right?.right).toBeUndefined();
 	});
 
 	test('nested', () => {
-		const pair = new Pair('[[[[1,2],[3,4]],[[5,6],[7,8]]],9]');
+		const pair = Pair.fromString('[[[[1,2],[3,4]],[[5,6],[7,8]]],9]');
 
 		expect(pair.value).toBeUndefined();
+		expect(pair.parent).toBeUndefined();
+		expect(pair.depth).toEqual(0);
 
 		expect(pair.left?.left?.left?.left?.value).toEqual(1);
+		expect(pair.left?.left?.left?.left?.depth).toEqual(4);
+		expect(pair.left?.left?.left?.left?.parent).toBe(pair.left?.left?.left);
+		expect(pair.left?.left?.left?.parent).toBe(pair.left?.left);
+		expect(pair.left?.left?.left?.depth).toEqual(3);
+		expect(pair.left?.left?.parent).toBe(pair.left);
+		expect(pair.left?.left?.depth).toEqual(2);
+		expect(pair.left?.parent).toBe(pair);
+		expect(pair.left?.depth).toEqual(1);
+
 		expect(pair.left?.left?.left?.right?.value).toEqual(2);
+		expect(pair.left?.left?.left?.right?.depth).toEqual(4);
+
 		expect(pair.left?.left?.right?.left?.value).toEqual(3);
+		expect(pair.left?.left?.right?.left?.depth).toEqual(4);
+
 		expect(pair.left?.left?.right?.right?.value).toEqual(4);
+		expect(pair.left?.left?.right?.right?.depth).toEqual(4);
+
 		expect(pair.left?.right?.left?.left?.value).toEqual(5);
+		expect(pair.left?.right?.left?.left?.depth).toEqual(4);
+
 		expect(pair.left?.right?.left?.right?.value).toEqual(6);
+		expect(pair.left?.right?.left?.right?.depth).toEqual(4);
+
 		expect(pair.left?.right?.right?.left?.value).toEqual(7);
+		expect(pair.left?.right?.right?.left?.depth).toEqual(4);
+
 		expect(pair.left?.right?.right?.right?.value).toEqual(8);
+		expect(pair.left?.right?.right?.right?.depth).toEqual(4);
 
 		expect(pair.right?.value).toEqual(9);
 		expect(pair.right?.left).toBeUndefined();
 		expect(pair.right?.right).toBeUndefined();
+		expect(pair.right?.parent).toBe(pair);
+		expect(pair.right?.depth).toEqual(1);
+	});
+
+	test('complicated', () => {
+		const pair = Pair.fromString('[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]');
+
+		expect(pair.left?.left?.left?.value).toEqual(9);
+		expect(pair.left?.left?.left?.depth).toEqual(3);
+
+		expect(pair.left?.left?.right?.left?.value).toEqual(3);
+		expect(pair.left?.left?.right?.left?.depth).toEqual(4);
+
+		expect(pair.left?.left?.right?.right?.value).toEqual(8);
+		expect(pair.left?.left?.right?.right?.depth).toEqual(4);
+
+		expect(pair.left?.right?.left?.left?.value).toEqual(0);
+		expect(pair.left?.right?.left?.left?.depth).toEqual(4);
+
+		expect(pair.left?.right?.left?.right?.value).toEqual(9);
+		expect(pair.left?.right?.left?.right?.depth).toEqual(4);
+
+		expect(pair.left?.right?.right?.value).toEqual(6);
+		expect(pair.left?.right?.right?.depth).toEqual(3);
+
+		expect(pair.right?.left?.left?.left?.value).toEqual(3);
+		expect(pair.right?.left?.left?.left?.depth).toEqual(4);
+
+		expect(pair.right?.left?.left?.right?.value).toEqual(7);
+		expect(pair.right?.left?.left?.right?.depth).toEqual(4);
+
+		expect(pair.right?.left?.right?.left?.value).toEqual(4);
+		expect(pair.right?.left?.right?.left?.depth).toEqual(4);
+
+		expect(pair.right?.left?.right?.right?.value).toEqual(9);
+		expect(pair.right?.left?.right?.right?.depth).toEqual(4);
+
+		expect(pair.right?.right?.value).toEqual(3);
+		expect(pair.right?.right?.depth).toEqual(2);
+	});
+});
+
+describe('leftmost/rightmost', () => {
+	test('simple', () => {
+		const pair = Pair.fromString('[1,2]');
+
+		expect(pair.leftmost()?.value).toEqual(1);
+		expect(pair.rightmost()?.value).toEqual(2);
+	});
+
+	test('complicated', () => {
+		const pair = Pair.fromString('[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]');
+
+		expect(pair.leftmost()?.value).toEqual(9);
+		expect(pair.rightmost()?.value).toEqual(3);
+
+		expect(pair.left?.leftmost()?.value).toEqual(9);
+		expect(pair.left?.rightmost()?.value).toEqual(6);
+
+		expect(pair.right?.leftmost()?.value).toEqual(3);
+		expect(pair.right?.rightmost()?.value).toEqual(3);
+
+		expect(pair.left?.right?.leftmost()?.value).toEqual(0);
+		expect(pair.left?.right?.rightmost()?.value).toEqual(6);
+	});
+});
+
+describe('next/prev', () => {
+	test('simple', () => {
+		const pair = Pair.fromString('[1,2]');
+
+		expect(pair.next()).toBeUndefined();
+		expect(pair.prev()).toBeUndefined();
+
+		expect(pair.left?.next()).toEqual(pair.right);
+		expect(pair.left?.prev()).toBeUndefined();
+
+		expect(pair.right?.next()).toBeUndefined();
+		expect(pair.right?.prev()).toEqual(pair.left);
+	});
+
+	test('complicated', () => {
+		const pair = Pair.fromString('[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]');
+		const ordered = [9,3,8,0,9,6,3,7,4,9,3];
+
+		let current = pair.leftmost();
+
+		for (const expected of ordered) {
+			expect(current?.value).toEqual(expected);
+			current = current?.next();
+		}
+	});
+});
+
+describe('magnitude', () => {
+	test('simple', () => {
+		const pair = Pair.fromString('[9,1]');
+		expect(pair.magnitude()).toEqual(29);
+	});
+
+	test('recursive', () => {
+		const pair = Pair.fromString('[[9,1],[1,9]]');
+		expect(pair.magnitude()).toEqual(129);
+	});
+});
+
+describe('add', () => {
+	test('simple', () => {
+		const added = Pair.add(Pair.fromString('[1,2]'), Pair.fromString('[[3,4],5]'));
+
+		expect(added.toString()).toEqual('[[1,2],[[3,4],5]]');
+		expect(added.left?.depth).toEqual(1);
+		expect(added.left?.left?.depth).toEqual(2);
+		expect(added.right?.left?.right?.depth).toEqual(3);
+		expect(added.right?.left?.right?.value).toEqual(4);
+	});
+
+	test('complicated', () => {
+		const added = Pair.add(Pair.fromString('[[[[4,3],4],4],[7,[[8,4],9]]]'), Pair.fromString('[1,1]'));
+
+		expect(added.toString()).toEqual('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]');
+	});
+});
+
+describe('explode', () => {
+	test('example 1', () => {
+		const pair = Pair.fromString('[[[[[9,8],1],2],3],4]');
+
+		const didExplode = pair?.left?.left?.left?.left?.explode();
+
+		expect(didExplode).toEqual(true);
+		expect(pair.toString()).toEqual('[[[[0,9],2],3],4]');
+	});
+
+	test('example 2', () => {
+		const pair = Pair.fromString('[7,[6,[5,[4,[3,2]]]]]');
+
+		const didExplode = pair?.right?.right?.right?.right?.explode();
+
+		expect(didExplode).toEqual(true);
+		expect(pair.toString()).toEqual('[7,[6,[5,[7,0]]]]');
+	});
+
+	test('example 3', () => {
+		const pair = Pair.fromString('[[6,[5,[4,[3,2]]]],1]');
+
+		const didExplode = pair?.left?.right?.right?.right?.explode();
+
+		expect(didExplode).toEqual(true);
+		expect(pair.toString()).toEqual('[[6,[5,[7,0]]],3]');
+	});
+
+	test('example 4', () => {
+		const pair = Pair.fromString('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]');
+
+		const didExplode = pair?.left?.right?.right?.right?.explode();
+
+		expect(didExplode).toEqual(true);
+		expect(pair.toString()).toEqual('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]');
+	});
+
+	test('example 5', () => {
+		const pair = Pair.fromString('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]');
+
+		const didExplode = pair?.right?.right?.right?.right?.explode();
+
+		expect(didExplode).toEqual(true);
+		expect(pair.toString()).toEqual('[[3,[2,[8,0]]],[9,[5,[7,0]]]]');
+	});
+
+	test('no explode needed', () => {
+		const pair = Pair.fromString('[[3,[2,[8,0]]],[9,[5,[7,0]]]]');
+
+		const didExplode = pair.explode();
+
+		expect(didExplode).toEqual(false);
+		expect(pair.toString()).toEqual('[[3,[2,[8,0]]],[9,[5,[7,0]]]]');
+	});
+});
+
+describe('split', () => {
+	test('example 1', () => {
+		const pair = Pair.fromString('10');
+
+		const didSplit = pair.split();
+
+		expect(didSplit).toEqual(true);
+		expect(pair.toString()).toEqual('[5,5]');
+	});
+
+	test('example 2', () => {
+		const pair = Pair.fromString('11');
+
+		const didSplit = pair.split();
+
+		expect(didSplit).toEqual(true);
+		expect(pair.toString()).toEqual('[5,6]');
+	});
+
+	test('example 3', () => {
+		const pair = Pair.fromString('12');
+
+		const didSplit = pair.split();
+
+		expect(didSplit).toEqual(true);
+		expect(pair.toString()).toEqual('[6,6]');
+	});
+
+	test('no split needed', () => {
+		const pair = Pair.fromString('9');
+
+		const didSplit = pair.split();
+
+		expect(didSplit).toEqual(false);
+		expect(pair.toString()).toEqual('9');
+	});
+});
+
+describe('reduce', () => {
+	test('explode only', () => {
+		// example 4 in explode tests above
+		const pair = Pair.fromString('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]');
+
+		let isReduced = pair.reduce(); // [7,3] reduced first
+
+		expect(isReduced).toEqual(true);
+		expect(pair.toString()).toEqual('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]');
+
+		isReduced = pair.reduce(); // [3,2] should be exploded next
+
+		expect(isReduced).toEqual(true);
+		expect(pair.toString()).toEqual('[[3,[2,[8,0]]],[9,[5,[7,0]]]]');
+
+		isReduced = pair.reduce(); // nothing more to reduce
+
+		expect(isReduced).toEqual(false);
+		expect(pair.toString()).toEqual('[[3,[2,[8,0]]],[9,[5,[7,0]]]]');
+	});
+
+	test('full example', () => {
+		const pair = Pair.add(Pair.fromString('[[[[4,3],4],4],[7,[[8,4],9]]]'), Pair.fromString('[1,1]'));
+		expect(pair.toString()).toEqual('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]');
+
+		let reduced = pair.reduce(); // explode
+		expect(reduced).toEqual(true);
+		expect(pair.toString()).toEqual('[[[[0,7],4],[7,[[8,4],9]]],[1,1]]');
+
+		reduced = pair.reduce(); // explode
+		expect(reduced).toEqual(true);
+		expect(pair.toString()).toEqual('[[[[0,7],4],[15,[0,13]]],[1,1]]');
+
+		reduced = pair.reduce(); // split
+		expect(reduced).toEqual(true);
+		expect(pair.toString()).toEqual('[[[[0,7],4],[[7,8],[0,13]]],[1,1]]');
+
+		reduced = pair.reduce(); // split
+		expect(reduced).toEqual(true);
+		expect(pair.toString()).toEqual('[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]');
+
+		reduced = pair.reduce(); // explode
+		expect(reduced).toEqual(true);
+		expect(pair.toString()).toEqual('[[[[0,7],4],[[7,8],[6,0]]],[8,1]]');
+
+		reduced = pair.reduce(); // nothing left to do
+		expect(reduced).toEqual(false);
+		expect(pair.toString()).toEqual('[[[[0,7],4],[[7,8],[6,0]]],[8,1]]');
 	});
 });
--- src/2021/day18/index.ts
+++ src/2021/day18/index.ts
@@ -2,27 +2,238 @@ import { Solver } from '../../common/Solver';
 import { InputParser}  from '../../common/InputParser';
 
 export class Pair {
-	public left: Pair | undefined;
-	public right: Pair | undefined;
+	public depth: number;
+	public parent: Pair | undefined;
+
+	// single-value pairs just have a value
 	public value: number | undefined;
 
-	constructor(line: string) {
+	// recursive pairs have a left and right pair
+	public left: Pair | undefined;
+	public right: Pair | undefined;
+
+	private constructor(parent?: Pair, value?: number) {
+		this.parent = parent || undefined;
+		this.depth = parent ? parent.depth + 1 : 0;
+		this.value = value || undefined;
+	}
+
+	public static fromString(line: string, parent?: Pair): Pair {
 		if (line.length === 0) {
 			throw 'empty line';
 		}
 
+		const pair = new Pair();
+
+		pair.parent = parent || undefined;
+		pair.depth = parent ? (parent.depth) + 1 : 0;
+
 		if (line[0] !== '[') {
-			this.value = parseInt(line);
-			if (isNaN(this.value)) {
+			pair.value = parseInt(line);
+			if (isNaN(pair.value)) {
 				throw 'unexpected value';
 			}
 		} else {
 			const inside = line.substr(1, line.length - 2);
 			const separator = Pair.findSeparator(inside);
 
-			this.left = new Pair(inside.substr(0, separator));
-			this.right = new Pair(inside.substr(separator + 1));
+			pair.left = Pair.fromString(inside.substr(0, separator), pair);
+			pair.right = Pair.fromString(inside.substr(separator + 1), pair);
 		}
+
+		return pair;
+	}
+
+	public static add(left: Pair, right: Pair): Pair {
+		const pair = new Pair();
+		pair.left = left;
+		pair.left.parent = pair;
+		pair.right = right;
+		pair.right.parent = pair;
+
+		left.incrementDepths();
+		right.incrementDepths();
+
+		return pair;
+	}
+
+	public static addAndReduce(left: Pair, right: Pair): Pair {
+		const pair = Pair.add(left, right);
+
+		let keepGoing = true;
+		while (keepGoing) {
+			keepGoing = pair.reduce();
+		}
+
+		return pair;
+	}
+
+	// this could be cached
+	public leftmost(): Pair | undefined {
+		if (this.left === undefined) {
+			return undefined;
+		}
+
+		let leftmost = this.left;
+		while (leftmost.left !== undefined) {
+			leftmost = leftmost.left;
+		}
+		return leftmost;
+	}
+
+	// this could be cached
+	public rightmost(): Pair | undefined {
+		if (this.right === undefined) {
+			return undefined;
+		}
+
+		let rightmost = this.right;
+		while (rightmost.right !== undefined) {
+			rightmost = rightmost.right;
+		}
+		return rightmost;
+	}
+
+	// this could be cached
+	public next(): Pair | undefined {
+		if (this.value === undefined || this.parent === undefined) {
+			return undefined;
+		}
+
+		// find first parent with a different right node
+		let nextParent: Pair | undefined = this.parent;
+		while (nextParent.rightmost() === this) {
+			if (nextParent.parent === undefined) {
+				return undefined;
+			}
+			nextParent = nextParent.parent;
+		}
+
+		// leftmost of the right of that one must be next
+		if (nextParent.right === undefined) {
+			return undefined;
+		} else if (nextParent.right.value !== undefined) {
+			return nextParent.right;
+		} else {
+			return nextParent.right.leftmost();
+		}
+	}
+
+	// this could be cached
+	public prev(): Pair | undefined {
+		if (this.value === undefined || this.parent === undefined) {
+			return undefined;
+		}
+
+		// find first parent with a different left node
+		let nextParent: Pair | undefined = this.parent;
+		while (nextParent.leftmost() === this) {
+			if (nextParent.parent === undefined) {
+				return undefined;
+			}
+			nextParent = nextParent.parent;
+		}
+
+		// rightmost of the left of that one must be next
+		if (nextParent.left === undefined) {
+			return undefined;
+		} else if (nextParent.left.value !== undefined) {
+			return nextParent.left;
+		} else {
+			return nextParent.left.rightmost();
+		}
+	}
+
+	// returns true if anything was changed
+	public reduce(): boolean {
+		// first try exploding all
+		let current = this.leftmost();
+		while (current !== undefined) {
+			// this should be a DFS instead of trying to process the same parent twice
+			if (current.parent && current.parent.explode()) {
+				return true;
+			}
+
+			current = current.next();
+		}
+
+		// then try splitting all
+		current = this.leftmost();
+		while (current !== undefined) {
+			if (current.split()) {
+				return true;
+			}
+
+			current = current.next();
+		}
+
+		return false;
+	}
+
+	// returns true if pair was exploded
+	public explode(): boolean {
+		if (this.depth < 4) {
+			return false;
+		}
+
+		const leftVal = this.left?.value;
+		const rightVal = this.right?.value;
+
+		if (leftVal === undefined || rightVal === undefined) {
+			return false;
+		}
+
+		const toAddLeft = this.left?.prev();
+		if (toAddLeft?.value !== undefined) {
+			toAddLeft.value += leftVal;
+		}
+
+		const toAddRight = this.right?.next();
+		if (toAddRight?.value !== undefined) {
+			toAddRight.value += rightVal;
+		}
+
+		this.value = 0;
+		this.left = undefined;
+		this.right = undefined;
+
+		return true;
+	}
+
+	// returns true if pair was split
+	public split(): boolean {
+		if (this.value === undefined || this.value < 10) {
+			return false;
+		}
+
+		const halfRoundedDown = Math.floor(this.value / 2);
+		const halfRoundedUp = Math.ceil(this.value / 2);
+
+		this.value = undefined;
+		this.left = new Pair(this, halfRoundedDown);
+		this.right = new Pair(this, halfRoundedUp);
+
+		return true;
+	}
+
+	public magnitude(): number {
+		if (this.value !== undefined) {
+			return this.value;
+		}
+
+		if (this.left === undefined || this.right === undefined) {
+			throw 'invalid pair';
+		}
+
+		return (this.left.magnitude() * 3) + (this.right.magnitude() * 2);
+	}
+
+	public toString(): string {
+		if (this.value !== undefined) {
+			return `${this.value}`;
+		}
+
+		return `[${this.left?.toString()},${this.right?.toString()}]`;
 	}
 
 	private static findSeparator(str: string): number {
@@ -41,8 +252,16 @@ export class Pair {
 		throw 'invalid line';
 	}
 
-	// todo - parent, next, prev, leftmost, rightmost, depth
-	// todo - split, explode, magnitude, private recalculateDepth
+	private incrementDepths(): void {
+		this.depth++;
+
+		if (this.left) {
+			this.left.incrementDepths();
+		}
+		if (this.right) {
+			this.right.incrementDepths();
+		}
+	}
 }
 
 class Day18Solver extends Solver {
@@ -53,11 +272,52 @@ class Day18Solver extends Solver {
 	}
 
 	protected solvePart1(): string {
-		return `${'todo'}`;
+		let sum = Pair.fromString(this.input[0]);
+
+		this.initProgress(this.input.length - 1);
+
+		for (let i = 1; i < this.input.length; i++) {
+			const toAdd = Pair.fromString(this.input[i]);
+			this.sampleLog(`Adding ${toAdd.toString()} to ${sum.toString()}`);
+
+			sum = Pair.addAndReduce(sum, toAdd);
+
+			this.sampleLog(`After sum: ${sum.magnitude()}, ${sum.toString()}`);
+			this.incrementProgress();
+		}
+
+		this.stopProgress();
+
+		return `${sum.magnitude()}`;
 	}
 
 	protected solvePart2(): string {
-		return `${'todo'}`;
+		let largest = 0;
+
+		this.initProgress(this.input.length * this.input.length);
+
+		for (let i = 0; i < this.input.length; i++) {
+			for (let j = 0; j < this.input.length; j++) {
+				if (i !== j) {
+					const left = Pair.fromString(this.input[i]);
+					const right = Pair.fromString(this.input[j]);
+					const sum = Pair.addAndReduce(left, right);
+					const magnitude = sum.magnitude();
+
+					largest = Math.max(magnitude, largest);
+
+					if (largest === magnitude) {
+						this.sampleLog(`Found new largest magnitude of ${magnitude} at indices ${i} and ${j}`);
+						this.sampleLog(`${left.toString()} + ${right.toString()} = ${sum.toString()}`);
+					}
+				}
+
+				this.incrementProgress();
+			}
+		}
+
+		this.stopProgress();
+		return `${largest}`;
 	}
 }
 
