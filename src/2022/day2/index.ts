5ee013d9db58f2e06af7e83bc690eb9e99048ee4
Nick Marsh <nickmarsh@gmail.com>
2022-12-02 15:41:05 +1000

    Day 2

--- src/2022/day2/index.ts
+++ src/2022/day2/index.ts
@@ -1,19 +1,130 @@
 import { Solver } from '../../common/Solver';
 import { InputParser}  from '../../common/InputParser';
 
+enum Hands {
+	ROCK,
+	PAPER,
+	SCISSORS,
+}
+enum Strat {
+	WIN,
+	LOSE,
+	DRAW,
+}
+
+const theirHands: {[x: string]: Hands} = {
+	'A': Hands.ROCK,
+	'B': Hands.PAPER,
+	'C': Hands.SCISSORS
+};
+const myHands: {[x: string]: Hands} = {
+	'X': Hands.ROCK,
+	'Y': Hands.PAPER,
+	'Z': Hands.SCISSORS
+};
+const strategies: {[x: string]: Strat} = {
+	'X': Strat.LOSE,
+	'Y': Strat.DRAW,
+	'Z': Strat.WIN
+};
+
 class Day2Solver extends Solver {
-	private input: Array<string> = [];
+	private input: Array<Array<string>> = [];
 
 	public init(inputFile: string): void {
-		this.input = InputParser.readLines(inputFile);
+		this.input = InputParser.readLinesAsChars(inputFile);
+	}
+
+	private handScore(hand: Hands): number {
+		switch (hand) {
+		case Hands.ROCK:
+			return 1;
+		case Hands.PAPER:
+			return 2;
+		case Hands.SCISSORS:
+			return 3;
+		default:
+			return 0;
+		}
+	}
+	
+	private winScore(them: Hands, me: Hands): number {
+		this.sampleLog(`them: ${them}, me: ${me}`);
+
+		if (me === them) {
+			return 3;
+		}
+		
+		if (
+			(me === Hands.ROCK && them === Hands.SCISSORS) || 
+			(me === Hands.PAPER && them === Hands.ROCK) || 
+			(me === Hands.SCISSORS && them === Hands.PAPER)
+		) {
+			return 6;
+		}
+
+		return 0;
+	}
+	
+	private handScoreToWin(hand: Hands): number {
+		switch (hand) {
+		case Hands.ROCK:
+			return this.handScore(Hands.PAPER);
+		case Hands.PAPER:
+			return this.handScore(Hands.SCISSORS);
+		case Hands.SCISSORS:
+			return this.handScore(Hands.ROCK);
+		default:
+			return 0;
+		}
+	}
+
+	private handScoreToLose(hand: Hands): number {
+		switch (hand) {
+		case Hands.ROCK:
+			return this.handScore(Hands.SCISSORS);
+		case Hands.PAPER:
+			return this.handScore(Hands.ROCK);
+		case Hands.SCISSORS:
+			return this.handScore(Hands.PAPER);
+		default:
+			return 0;
+		}
 	}
 
 	protected solvePart1(): string {
-		return `${'todo'}`;
+		this.sampleLog(this.input);
+
+		let score = 0;
+		this.input.forEach(line => {
+			const myHand = myHands[line[2]];
+			const theirHand = theirHands[line[0]];
+			score += this.handScore(myHand) + this.winScore(theirHand, myHand);
+		});
+
+		return `${score}`;
 	}
 
 	protected solvePart2(): string {
-		return `${'todo'}`;
+		let score = 0;
+		this.input.forEach(line => {
+			const strategy = line[2];
+			const theirHand = theirHands[line[0]];
+
+			switch (strategies[strategy]) {
+			case Strat.WIN:
+				score += 6 + this.handScoreToWin(theirHand);
+				break;
+			case Strat.LOSE:
+				score += 0 + this.handScoreToLose(theirHand);
+				break;
+			case Strat.DRAW:
+				score += 3 + this.handScore(theirHand);
+				break;
+			}
+		});
+
+		return `${score}`;
 	}
 }
 
